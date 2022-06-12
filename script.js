ef6ac7522fa16223afd74c953cff8e489a3a8153
kmitova <96522051+kmitova@users.noreply.github.com>
2022-06-12 16:54:00 +0300

    cleanup and formatting

--- script.js
+++ script.js
@@ -1,41 +1,26 @@
-let count = 1;
 const cells = Array.from(document.getElementsByClassName("cell"));
 const board = document.querySelector(".board");
+const restartButton = document.getElementById("restart-btn");
 let result = document.getElementById("result");
 let thereIsWinner = false;
 let thereIsATie = false;
 let chosenCell;
 let player1Selections = [];
 let player2Selections = [];
-let winner;
-let restartButton = document.getElementById("restart-btn");
+let count = 1;
 
 result.textContent = "Player 1 turn!";
 restartButton.addEventListener("click", () => {
   location.reload();
 });
-// hide board when game is over
-// if game is over, restart button shows up, when pressed, it reloads the site and sets board display to visible
 
-// while loop breaks live server
-// while (thereIsWinner === false && thereIsATie === false) {
 cells.forEach((cell) =>
   cell.addEventListener("click", (e) => {
     chosenCell = e.target.id;
-    // check if there is space function
-
-    // checkForSpace()
-    // play function if there is space and no winner
     if (!thereIsATie && !thereIsWinner) {
       play(chosenCell);
-      // checkWinner()
     }
-    // checkwinner function
     if (thereIsWinner || thereIsATie) {
-      // board.classList.add('hide')
-      // board.css.display = 'none'
-
-      // display game over somewhere and play again button which will reload the page
       if (thereIsWinner) {
         if (count % 2 === 0) {
           result.textContent = "Player 1 wins the game";
@@ -50,16 +35,11 @@ cells.forEach((cell) =>
     }
   })
 );
-// }
 
 function play(selection) {
-  console.log("in play function");
   let choice = document.getElementById(selection);
-
   if (!thereIsWinner) {
     checkForSpace();
-    console.log(thereIsATie);
-
     if (count % 2 !== 0 && choice.textContent === "") {
       result.textContent = "Player 2 turn!";
       choice.textContent = "X";
@@ -73,14 +53,10 @@ function play(selection) {
   checkWinner();
   if (thereIsWinner) {
     thereIsATie = false;
-    console.log("there is a winner");
-  } else if (thereIsATie) {
-    console.log("it is a tie");
   }
   count += 1;
 }
 
-// function check if there is space
 function checkForSpace() {
   filledCells = 1;
   for (let cell of cells) {
@@ -91,14 +67,10 @@ function checkForSpace() {
       thereIsATie = true;
     }
   }
-  console.log(filledCells);
-  console.log(thereIsATie);
 }
-// function check if there is a winner
+
 function checkWinner() {
-  // console.log(player1Selections)
-  // console.log(player2Selections)
-  // checking player 1:
+  // check player 1
   if (
     player1Selections.includes("1") &&
     player1Selections.includes("2") &&
